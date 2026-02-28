4a85be322b28cb682cfdde63f47a61098a56f925
Tanav Poswal <tanavposwal939@gmail.com>
2026-02-28 15:11:59 +0530

    Init - better-auth - prisma - shadcn

---
+++ lib/auth.ts
@@ -0,0 +1,17 @@
+import { betterAuth } from "better-auth";
+import { nextCookies } from "better-auth/next-js";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { prisma } from "./prisma";
+
+export const auth = betterAuth({
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    },
+  },
+  plugins: [nextCookies()],
+  database: prismaAdapter(prisma, {
+    provider: "postgresql",
+  }),
+});
